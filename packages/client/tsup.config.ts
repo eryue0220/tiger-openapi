c59e391dac5fa66cb72e1824fc48b847874589fc
eryue0220 <eryue0220@gmail.com>
2026-03-07 16:01:51 +0800

    ci

--- packages/client/tsup.config.ts
+++ packages/client/tsup.config.ts
@@ -1,13 +1,5 @@
 import { defineConfig } from 'tsup';
 
-const alias = {
-  'tiger-openapi-core': '../core/src/index.ts',
-  'tiger-openapi-http': '../http/src/index.ts',
-  'tiger-openapi-pb': '../pb/src/index.ts',
-  'tiger-openapi-shared': '../shared/src/index.ts',
-  'tiger-openapi-stream': '../stream/src/index.ts',
-};
-
 export default defineConfig({
   entry: {
     index: 'src/index.ts',
@@ -20,6 +12,5 @@ export default defineConfig({
   sourcemap: true,
   clean: true,
   target: 'es2022',
-  alias,
   noExternal: [/tiger-openapi-/],
 });
